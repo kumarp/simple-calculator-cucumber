00795333c6b25b056b87dfb0daf36aefccb4c5dc
Priti Kumar <pritikumar3@gmail.com>
2020-03-13 20:50:35 -0400

    Updated to Java 13, Junit 4.13 and Cucumber 5.5.0

--- simple-calculator/src/test/java/org/example/calculator/cucumber/runners/CalculatorScenarioRunnerTest.java
+++ simple-calculator/src/test/java/org/example/calculator/cucumber/runners/CalculatorScenarioRunnerTest.java
@@ -2,11 +2,10 @@ package org.example.calculator.cucumber.runners;
 
 import org.junit.runner.RunWith;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features", glue = "org.example.calculator.cucumber.steps", strict = true)
 public class CalculatorScenarioRunnerTest {
-
 }
--- simple-calculator/src/test/java/org/example/calculator/cucumber/steps/CalculatorSteps.java
+++ simple-calculator/src/test/java/org/example/calculator/cucumber/steps/CalculatorSteps.java
@@ -7,63 +7,62 @@ import java.util.Map;
 
 import org.example.calculator.handler.CalculatorHandler;
 
-import cucumber.api.DataTable;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class CalculatorSteps {
 
-		int operand1;
-	 	int operand2;
-	 	int result;
-	 
-	 	CalculatorHandler calculatorHandler = new CalculatorHandler();
-	 
-	 	@Given("^the following calculator input:$")
-	 	public void the_following_calculator_input(
-	 			DataTable calculatorInputDataTable) throws Throwable {
-	 		List<Map<String, String>> rows = calculatorInputDataTable.asMaps();
-	 		for (Map<String, String> row : rows) {
-	 			operand1 = Integer.parseInt(row.get("Operand 1"));
-	 			operand2 = Integer.parseInt(row.get("Operand 2"));
-	 		}
-	 	}
-	 
-	 	@When("^I do an? addition calculation$")
-	 	public void I_do_a_addition_calculation() throws Throwable {
-	 		result = calculatorHandler.doAddition(operand1, operand2);
-	 	}
-	 
-	 	@When("^I do a subtraction calculation$")
-	 	public void I_do_a_subtraction_calculation() throws Throwable {
-	 		result = calculatorHandler.doSubtraction(operand1, operand2);
-	 	}
-	 
-	 	@When("^I do a multiplication calculation$")
-	 	public void I_do_a_multiplication_calculation() throws Throwable {
-	 		result = calculatorHandler.doMultiplication(operand1, operand2);
-	 	}
-	 
-	 	@When("^I do a division calculation$")
-	 	public void I_do_a_division_calculation() throws Throwable {
-	 		result = calculatorHandler.doDivision(operand1, operand2);
-	 	}
-	 
-	 	@Then("^I expect the result \"([^\"]*)\"$")
-	 	public void I_expect_the_result(String expectedResult) throws Throwable {
-	 		assertTrue(result == Float.parseFloat(expectedResult));
-	 	}
-	 	
-	 	@Then("^I expect the result (\\d+)$")
-	 	public void I_expect_the_result(int expectedResult) throws Throwable {
-	 		assertTrue(expectedResult == result);
-	 	}
+	int operand1;
+	int operand2;
+	int result;
 
-	 	@Then("^I expect the result -(\\d+)$")
-	 	public void I_expect_the_result_(int expectedResult) throws Throwable {
-	 		expectedResult = expectedResult * -1;
-	 		assertTrue(expectedResult == result);
-	 	}
+	CalculatorHandler calculatorHandler = new CalculatorHandler();
+
+	@Given("^the following calculator input:$")
+	public void the_following_calculator_input(DataTable calculatorInputDataTable) throws Throwable {
+		List<Map<String, String>> rows = calculatorInputDataTable.asMaps();
+		for (Map<String, String> row : rows) {
+			operand1 = Integer.parseInt(row.get("Operand 1"));
+			operand2 = Integer.parseInt(row.get("Operand 2"));
+		}
+	}
+
+	@When("^I do an? addition calculation$")
+	public void I_do_a_addition_calculation() throws Throwable {
+		result = calculatorHandler.doAddition(operand1, operand2);
+	}
+
+	@When("^I do a subtraction calculation$")
+	public void I_do_a_subtraction_calculation() throws Throwable {
+		result = calculatorHandler.doSubtraction(operand1, operand2);
+	}
+
+	@When("^I do a multiplication calculation$")
+	public void I_do_a_multiplication_calculation() throws Throwable {
+		result = calculatorHandler.doMultiplication(operand1, operand2);
+	}
+
+	@When("^I do a division calculation$")
+	public void I_do_a_division_calculation() throws Throwable {
+		result = calculatorHandler.doDivision(operand1, operand2);
+	}
+
+	@Then("^I expect the result \"([^\"]*)\"$")
+	public void I_expect_the_result(String expectedResult) throws Throwable {
+		assertTrue(result == Float.parseFloat(expectedResult));
+	}
+
+	@Then("^I expect the result (\\d+)$")
+	public void I_expect_the_result(int expectedResult) throws Throwable {
+		assertTrue(expectedResult == result);
+	}
+
+	@Then("^I expect the result -(\\d+)$")
+	public void I_expect_the_result_(int expectedResult) throws Throwable {
+		expectedResult = expectedResult * -1;
+		assertTrue(expectedResult == result);
+	}
 
 }
